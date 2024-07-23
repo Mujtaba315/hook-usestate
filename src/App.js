9e3aadef424c7d8b2636285f7620de25e696e4df
Syed Muhammad Mujtaba Naqvi <123009259+Mujtaba315@users.noreply.github.com>
2024-07-23 06:50:45 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useState } from 'react'; 
+import Counter from './components/Counter';
+import Timer from './components/Timer';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  
+  function increment () {
+    setCount(count + 1);
+  }
+
+  function decrement () {
+    setCount(count - 1)
+  }
+
+  function currentTime () {
+    setTime(new Date().toLocaleTimeString());
+    setInterval(currentTime, 1000);
+  }
+
+  
+
+  return (
+    <>
+      <Counter 
+        count={count}
+        increment={increment}
+        decrement={decrement}
+      />
+
+      <Timer 
+        time={time}
+        currentTime={currentTime}
+      />
+
+    </>
+  );
+}
+
+export default App;
